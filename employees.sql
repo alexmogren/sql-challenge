695e590614f066d40a2fa69bd976da8cefef0505
Alexander Mogren <alexandermogren@Alexanders-MacBook-Pro.local>
2020-08-15 14:21:28 -0500

    sql hw

---
+++ employees.sql
@@ -0,0 +1,77 @@
+-- employee details
+SELECT  emp.emp_no,
+        emp.last_name,
+        emp.first_name,
+        emp.sex,
+        sal.salary
+FROM employees as emp
+    LEFT JOIN salaries as sal
+    ON (emp.emp_no = sal.emp_no)
+ORDER BY emp.emp_no;
+
+-- employees hired by company in 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+-- find Manager information of each department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        e.last_name,
+        e.first_name
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN employees AS e
+        ON (dm.emp_no = e.emp_no);
+
+-- find department for each employee
+SELECT  e.emp_no,
+        e.last_name,
+        e.first_name,
+        d.dept_name
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+ORDER BY e.emp_no;
+
+-- first & last name & sex for employee whose name is "Hercules" and last name begins with "B"
+SELECT first_name, last_name, birth_date, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
+
+-- List all employees with details in the sales department
+SELECT  e.emp_no,
+        e.last_name,
+        e.first_name,
+        d.dept_name
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+WHERE d.dept_name = 'Sales'
+ORDER BY e.emp_no;
+
+-- List all employees with details in sales and development departments
+SELECT  e.emp_no,
+        e.last_name,
+        e.first_name,
+        d.dept_name
+FROM employees AS e
+    INNER JOIN dept_emp AS de
+        ON (e.emp_no = de.emp_no)
+    INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+WHERE d.dept_name IN ('Sales', 'Development')
+ORDER BY e.emp_no;
+
+-- The frequency of shared employee last names in descending order
+SELECT last_name, COUNT(last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY COUNT(last_name) DESC;
